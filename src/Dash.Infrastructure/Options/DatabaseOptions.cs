574cb9a4c1a0ee9708bcfb533da0d42815bdfde5
Halfdan Sand Christensen <halfdansc@proton.me>
2026-01-12 16:04:01 +0100

    database options

---
+++ src/Dash.Infrastructure/Options/DatabaseOptions.cs
@@ -0,0 +1,9 @@
+namespace Dash.Infrastructure.Options;
+
+public class DatabaseOptions
+{
+    public const string SectionName = "Database";
+
+    public required string Provider { get; set; }
+    public required string ConnectionString { get; set; }
+}
