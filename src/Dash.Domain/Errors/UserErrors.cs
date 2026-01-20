2e1ae34115f8828eca5fee44b107fb9458f2b030
Halfdan Sand Christensen <halfdansc@proton.me>
2026-01-20 13:38:11 +0100

    domain specifc user errors

---
+++ src/Dash.Domain/Errors/UserErrors.cs
@@ -0,0 +1,20 @@
+using Dash.Domain.Common;
+
+namespace Dash.Domain.Errors;
+
+public static class UserErrors
+{
+    public static readonly Error EmailAlreadyInUse = new(
+        "User.EmailInUse",
+        "The provided email is already registered.");
+
+
+    public static readonly Error UsernameAlreadyInUse = new(
+        "User.UsernameInUse",
+        "The provided username is already taken.");
+
+
+    public static readonly Error InvalidCredentials = new(
+        "User.InvalidCredentials",
+        "The email/username or password provided is incorrect.");
+}
