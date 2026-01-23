f4984d7f375c01ca22efe17cb2dd1a81bad07b7e
Halfdan Sand Christensen <halfdansc@proton.me>
2026-01-23 08:51:14 +0100

    end point structure

--- src/Dash.Api/Features/Authentication/AuthEndpoints.cs
+++ src/Dash.Api/Features/Authentication/AuthEndpoints.cs
@@ -1,6 +1,34 @@
+using Dash.Domain.Common;
+using Dash.Application.Features.Authentication.DTOs;
+using Dash.Application.Features.Authentication.Interfaces;
+using Microsoft.AspNetCore.Http.HttpResults;
+
 namespace Dash.Api.Features.Authentication;
 
-public class AuthEndPoints
+internal static class AuthEndpoints
 {
+    public static IEndpointRouteBuilder MapAuthEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/api/auth")
+            .WithTags("Authentication");
 
+        group.MapPost("/login", LoginAsync);
+        group.MapPost("/register", RegisterAsync);
+
+        return app;
+    }
+
+    private static async Task<Results<Ok<AuthResponse>, BadRequest<Error>>> LoginAsync(
+        LoginRequest request,
+        IAuthService authService)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static async Task<Results<Ok<AuthResponse>, BadRequest<Error>>> RegisterAsync(
+            LoginRequest request,
+            IAuthService authService)
+    {
+        throw new NotImplementedException();
+    }
 }
