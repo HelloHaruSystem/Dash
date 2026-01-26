a3cc637a2e48c787c8a7a4edfb2764dcf6f66024
Halfdan Sand Christensen <halfdansc@proton.me>
2026-01-26 12:14:45 +0100

    root endpoint test

--- tests/Dash.Api.Tests/Integration/ProgramTests.cs
+++ tests/Dash.Api.Tests/Integration/ProgramTests.cs
@@ -1,6 +1,24 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
 namespace Dash.Api.Tests.Integration;
 
-public class ProgramTests
+public class ProgramTests : IClassFixture<WebApplicationFactory<Program>>
 {
+    private readonly HttpClient _client;
 
+    public ProgramTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Root_Should_returnHelloWorld()
+    {
+        var response = await _client.GetAsync("/");
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        string content = await response.Content.ReadAsStringAsync();
+        Assert.Contains("Hello, World!", content);
+    }
 }
