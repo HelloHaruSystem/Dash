a8a2fdfe3e3dbd281e6548c39c854355d6efd6a2
Halfdan Sand Christensen <halfdansc@proton.me>
2026-02-05 11:21:39 +0100

    added constructore to login attempts repo

--- src/Dash.Infrastructure/Persistence/Repositories/LoginAttemptRepository.cs
+++ src/Dash.Infrastructure/Persistence/Repositories/LoginAttemptRepository.cs
@@ -1,10 +1,23 @@
 using Dash.Application.Common.Persistence;
 using Dash.Domain.Entities;
+using Dash.Infrastructure.Data;
+using Microsoft.Extensions.Logging;
 
 namespace Dash.Infrastructure.Persistence.Repositories;
 
 public sealed class LoginAttemptRepository : ILoginAttemptRepository
 {
+    private readonly DashDbContext _context;
+    private readonly ILogger<LoginAttemptRepository> _logger;
+
+    public LoginAttemptRepository(
+        DashDbContext context,
+        ILogger<LoginAttemptRepository> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
     public Task AddAsync(LoginAttempt loginAttempt)
     {
         throw new NotImplementedException();
