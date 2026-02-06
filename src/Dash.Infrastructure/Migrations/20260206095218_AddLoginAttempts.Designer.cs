ddd139d8474b2fd394cbcb76c9a6e85b3479b3ce
Halfdan Sand Christensen <halfdansc@proton.me>
2026-02-06 10:53:33 +0100

    database migrations

--- src/Dash.Infrastructure/Migrations/20260206095218_AddLoginAttempts.Designer.cs
+++ src/Dash.Infrastructure/Migrations/20260206095218_AddLoginAttempts.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Dash.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Dash.Infrastructure.Migrations
 {
     [DbContext(typeof(DashDbContext))]
-    partial class DashDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260206095218_AddLoginAttempts")]
+    partial class AddLoginAttempts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.2");
---
+++ src/Dash.Infrastructure/Migrations/20260206095218_AddLoginAttempts.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dash.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLoginAttempts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
