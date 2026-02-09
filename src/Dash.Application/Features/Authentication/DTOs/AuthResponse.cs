0e2c74a23f1ade6f2bd2128a3a8f204ab87dc646
Halfdan Sand Christensen <halfdansc@proton.me>
2026-02-09 21:19:16 +0100

    updated AuthResponse DTO to include refresh token

--- src/Dash.Application/Features/Authentication/DTOs/AuthResponse.cs
+++ src/Dash.Application/Features/Authentication/DTOs/AuthResponse.cs
@@ -6,4 +6,5 @@ public sealed record AuthResponse
     public required string Username { get; init; }
     public required string Email { get; init; }
     public required string Token { get; init; }
+    public required string Refreshtoken { get; init; }
 }
