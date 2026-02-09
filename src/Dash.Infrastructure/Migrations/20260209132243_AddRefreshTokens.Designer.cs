8d15fe7ea172441f8a2d2cc5e2ecc6d43d15a530
Halfdan Sand Christensen <halfdansc@proton.me>
2026-02-09 14:23:07 +0100

    refresh token migrations

--- src/Dash.Infrastructure/Migrations/20260209132243_AddRefreshTokens.Designer.cs
+++ src/Dash.Infrastructure/Migrations/20260209132243_AddRefreshTokens.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Dash.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Dash.Infrastructure.Migrations
 {
     [DbContext(typeof(DashDbContext))]
-    partial class DashDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260209132243_AddRefreshTokens")]
+    partial class AddRefreshTokens
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.2");
