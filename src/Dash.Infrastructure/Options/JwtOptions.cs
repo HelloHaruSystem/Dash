87233e63e9a972bcddbbc76dfd3be5608414c319
Halfdan Sand Christensen <halfdansc@proton.me>
2026-02-09 21:22:50 +0100

    updated options for refresh token

--- src/Dash.Infrastructure/Options/JwtOptions.cs
+++ src/Dash.Infrastructure/Options/JwtOptions.cs
@@ -8,4 +8,5 @@ public sealed class JwtOptions
     public required string Issuer { get; set; }
     public required string Audience { get; set; }
     public required int ExpiresInMinutes { get; set; }
+    public required int RefreshTokenExpiresInDays { get; set; }
 }
