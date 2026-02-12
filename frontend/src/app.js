ab769054b7920136a354ceab9d98a0a03cd19e5f
Halfdan Sand Christensen <halfdansc@proton.me>
2026-02-12 13:30:03 +0100

    small changes

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,19 +1,55 @@
-const login = () => {};
+const authBaseUrl = "https://127.0.0.1:7261/api/auth";
 
-const register = () => {};
+const login = async (identifier, password) => {
+  const loginUrl = authBaseUrl + "/login";
+  const request = new LoginRequest(identifier, password);
 
-const AuthResponse = (id, username, email, token, refreshToken) => {
+  const response = await fetch(loginUrl, {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(request),
+  });
+};
+
+const register = () => {
+  const registerUrl = authBaseUrl + "/register";
+};
+
+const refresh = () => {
+  const refreshUrl = authBaseUrl + "/refresh";
+};
+
+const aboutMe = () => {
+  const aboutMeUrl = () => authBaseUrl + "/test/me";
+};
+
+function AuthResponse(id, username, email, token, refreshToken) {
   this.id = id;
   this.username = username;
   this.email = email;
   this.token = token;
   this.refreshToken = refreshToken;
-};
+}
 
-const LoginRequest = (identifier, password) => {};
+function LoginRequest(identifier, password) {
+  this.identifier = identifier;
+  this.password = password;
+}
 
-const RegisterRequest = (username, email, password) => {};
+function RegisterRequest(username, email, password) {
+  this.username = username;
+  this.email = email;
+  this.password = password;
+}
 
-const RefreshRequest = (refreshToken) => {};
+function RefreshRequest(refreshToken) {
+  this.refreshToken = refreshToken;
+}
 
-const Error = (code, description) => {};
+function Error(code, description) {
+  this.code = code;
+  this.description = description;
+}
